6b6bfc9014971f7e7c429cdb60f465cc086e028d
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-09-21 11:56:26 +0800

    init

---
+++ deploy.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const {spawn} = require('child_process');
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({
+    path: '/admin/push',
+    secret: 'jianfeng19901009'
+});
+http.createServer((req, res) => {
+    handler(req,res, (err) => {
+        console.log(err)
+        res.statusCode = 404;
+        res.end("no such location");
+    });
+}).listen(3000);
+handler.on('error', (err) => {
+    console.error('Error:', err.message);
+});
+handler.on('push', (event) => {
+    console.log('Received a push event for %s to %s', event.payload.repositpry.name, event.payload.ref);
+    runCommand('sh', ['./deploy.sh'], (txt) => {
+        console.log(txt)
+    });
+});
+function runCommand(cmd, args, callback) {
+    const child = spawn(cmd, args);
+    var response = '';
+    child.stdout.on('data', (buffer) => {
+        response += buffer.toString();
+    });
+    child.stdout.on('end', () => {
+        callback(response);
+    });
+}
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+git checkout masterl
+git pull;
