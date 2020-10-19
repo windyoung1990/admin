79924aedc5b16daee228ec0c2cc50451a37c9d62
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-10-19 18:18:50 +0800

    change path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       host : '62.234.27.149',
       ref  : 'origin/master',
       repo : 'https://github.com/windyoung1990/admin.git',
-      path : '/home/project/admin2',
+      path : '/home/project/admin',
       'pre-deploy-local': '',
       'post-deploy' : 'git pull origin master && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
