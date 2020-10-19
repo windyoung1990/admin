2447f5b8c85f3b48e62c7140f4062c592625b651
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-10-19 18:05:51 +0800

    update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       repo : 'https://github.com/windyoung1990/admin.git',
       path : '/home/project/admin2',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run build && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'git pull origin master && npm install && npm run build && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
