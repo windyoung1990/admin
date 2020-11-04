5baff82ed8be3a5f7771a69a1d684c981a251184
风少 <fengshao@fengshaodeMacBook-Pro.local>
2020-11-04 18:00:23 +0800

    修改assetsDir字段

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve(dir) {
 module.exports = {
     lintOnSave: false,
     publicPath: '/admin/',
-    assetDir: 'admin',
+    assetsDir: 'admin',
     chainWebpack: (config) => {
         config.resolve.alias = Object.assign(config.resolve.alias, {
             '@/src': resolve('src'),
